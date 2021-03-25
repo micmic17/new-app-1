6dcbafaf72cec45339e2b543309bb6e6213513b5
micmic17 <mickale_saturre@ligph.com>
2021-03-25 11:02:45 +0800

    Added initial pages

---
+++ new-app-1/Views/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  new-app-1
+//
+//  Created by Mickale Saturre on 3/25/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ new-app-1/Views/LoginView.swift
@@ -0,0 +1,25 @@
+//
+//  LoginView.swift
+//  new-app-1
+//
+//  Created by Mickale Saturre on 3/25/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                
+            }
+        }
+    }
+}
+
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
